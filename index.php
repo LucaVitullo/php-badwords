5a88dc0e2b2077bea30ae10077f12ba69e0d6250
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-05-02 17:30:05 +0200

    ex completato

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/* Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+*/
+$name = 'php';
+$paragrafo = 'ciao mi chiamo ' . $name;
+$censura = $_GET['php'];
+$sostituto = str_replace($name, "***", $paragrafo);
+?>
+<h1>Parola Originale</h1>
+<h3><?php echo $paragrafo ?></h3>
+<h3>Lunghezza: <?php echo strlen($paragrafo) ?></h3>
+
+<h1>Parola da censurare</h1>
+<h3><?php echo ($sostituto) ?></h3>
+<h3>Lunghezza: <?php echo strlen($sostituto) ?></h3>
